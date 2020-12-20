1538390d67a7d1fe09a3a32d5e40501daa6f5c5d
bidetaggle <bidetaggle@xxx>
2020-12-20 10:46:37 +0100

    password.py renaming + full unit test

--- sample/password.py
+++ sample/password.py
@@ -5,29 +5,29 @@ def generate(masterkey, domain):
     pk = upw.derive_key_from(masterkey, domain)
     pkResized = pk[0:cfg.get('passwords_length')]
 
-    c = dict(digit=0, letter=0)
+    counters = dict(digit=0, letter=0)
     password = ''
     for char in pkResized:
         if char.isdigit():
-            password += process_digits(char, c["digit"])
-            c["digit"] += 1
+            password += process_digit(char, counters["digit"])
+            counters["digit"] += 1
         else:
-            password += process_letters(char, c["letter"])
-            c["letter"] += 1
+            password += process_letter(char, counters["letter"])
+            counters["letter"] += 1
 
     return password
 
-def process_letters(letter, c):
+def process_letter(letter, n):
     # Uppercase each 2 letters
-    if c % 2 == 1:
+    if n % 2 == 1:
         return letter.upper()
     else:
         return letter
 
-def process_digits(digit, c):
+def process_digit(digit, n):
     # replace each 2 digits by the next special character from the config.yml list
-    if c % 2 == 1:
+    if n % 2 == 1:
         specialCharList = cfg.get("spec_char_list")
-        return specialCharList[math.ceil(c / 2) - 1]
+        return specialCharList[math.ceil(n / 2) - 1]
     else:
         return digit
--- tests/test_password.py
+++ tests/test_password.py
@@ -3,8 +3,44 @@ from sample import password
 
 class generate(unittest.TestCase):
 
-    def test_passwordFormat(self):
+    def test_password_format(self):
         self.assertTrue(password.generate('masterkey', 'domain.ltd') == 'f9D#d4%B8$fA2^e7')
+
+class process_letter(unittest.TestCase):
+
+    def test_letter_unchanged(self):
+        self.assertEqual(password.process_letter('a', 0), 'a')
+        self.assertEqual(password.process_letter('b', 2), 'b')
+        self.assertEqual(password.process_letter('c', 6), 'c')
+
+    def test_letter_to_uppercase(self):
+        self.assertEqual(password.process_letter('d', 1), 'D')
+        self.assertEqual(password.process_letter('e', 3), 'E')
+        self.assertEqual(password.process_letter('f', 9), 'F')
+
+class process_digit(unittest.TestCase):
+
+    def test_digit_unchanged(self):
+        self.assertEqual(password.process_digit('1', 0), '1')
+        self.assertEqual(password.process_digit('2', 2), '2')
+        self.assertEqual(password.process_digit('2', 4), '2')
+        self.assertEqual(password.process_digit('2', 6), '2')
+        self.assertEqual(password.process_digit('3', 8), '3')
+        self.assertEqual(password.process_digit('3', 10), '3')
+        self.assertEqual(password.process_digit('3', 12), '3')
+        self.assertEqual(password.process_digit('3', 14), '3')
+        self.assertEqual(password.process_digit('3', 16), '3')
+
+    def test_digit_to_special_char(self):
+        self.assertEqual(password.process_digit('1', 1), '#')
+        self.assertEqual(password.process_digit('2', 3), '%')
+        self.assertEqual(password.process_digit('3', 5), '$')
+        self.assertEqual(password.process_digit('4', 7), '^')
+        self.assertEqual(password.process_digit('5', 9), '@')
+        self.assertEqual(password.process_digit('6', 11), '?')
+        self.assertEqual(password.process_digit('7', 13), '!')
+        self.assertEqual(password.process_digit('8', 15), '&')
+        self.assertEqual(password.process_digit('9', 17), '*')
 
 if __name__ == '__main__':
     unittest.main()
